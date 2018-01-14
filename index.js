92e04768d150fffa30bb89ca7378736a8c438877
Matt <mmszek3000@gmail.com>
2018-01-14 07:23:39 -0500

    Add basic stuff

---
+++ index.js
@@ -0,0 +1,37 @@
+
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    if (message.substring(0, 1) == '%') {
+        var args = message.substring(1).split('|');
+        var cmd = message.substring(1).split(' ')[0];
+        args.unshift(message.substring(1).split(' ')[1])
+
+        switch(cmd) {
+            // %hello
+            case 'hello':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Hi there!'
+                });
+                break;
+         }
+     }
+});
