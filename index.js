2e77569e853c1a70b4ff588a46cb2dc463b22600
Matt <mmszek3000@gmail.com>
2018-01-19 18:23:20 -0500

    Remove %banmyself and resolve #1

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const Discord = require('discord.io');
 const logger = require('winston');
 const auth = require('./auth.json');
-const {listOfMons} = require('./listofmons.json')
+const { listOfMons } = require('./listofmons.json');
 
-var helpIcon = {url: 'https://housing.umn.edu/sites/housing.umn.edu/files/help.png', width: 200, height: 200};
+var helpIcon = {
+    url: 'https://housing.umn.edu/sites/housing.umn.edu/files/help.png',
+    width: 200,
+    height: 200
+};
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -15,18 +19,18 @@ var bot = new Discord.Client({
     token: auth.token,
     autorun: true
 });
-bot.on('ready', (evt) => {
+bot.on('ready', evt => {
     void evt;
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
-    bot.setPresence({'game': {'name': '%help'}});
+    bot.setPresence({ game: { name: '%help' } });
 });
 bot.on('disconnect', () => {
     logger.warn('Bot disconnected');
     bot.connect();
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', function(user, userID, channelID, message, evt) {
     void evt;
     if (message.substring(0, 1) == '%') {
         var args = message.substring(1).split('|');
@@ -36,13 +40,13 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         // trim whitespace
         if (args[0]) {
             for (let i = 0; i < args.length; i++) {
-                args[i] =  args[i].trim();
+                args[i] = args[i].trim();
             }
         } else {
             args = [];
         }
 
-        switch(cmd) {
+        switch (cmd) {
         // %hello
         case 'hello':
             bot.sendMessage({
@@ -50,23 +54,18 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 message: 'Hi there!'
             });
             break;
-            // %banmyself
-        case 'banmyself':
-            try {
-                var serverID = bot.channels[channelID].guild_id;
-                bot.sendMessage({to: userID, message: 'As you wish.'});
-                bot.ban({serverID, userID});
-            } catch (e) {
-                bot.sendMessage({to: channelID, message: 'Unable to ban here.'});
-            }
-            break;
+
         // %randmon
         case 'randmon':
             bot.sendMessage({
                 to: channelID,
-                message: 'Your random Pokemon is: **'+listOfMons[Math.floor(Math.random()*807)]+'**.'
+                message:
+                        'Your random Pokemon is: **' +
+                        listOfMons[Math.floor(Math.random() * 807)] +
+                        '**.'
             });
             break;
+
         // %help
         case 'help':
             switch (args[0]) {
@@ -76,84 +75,89 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: null,
                     embed: {
-                        'authorname': 'Bot Help',
-                        'title': 'Command: %help',
-                        'thumbnail': helpIcon,
-                        'fields': [
-                            {name: 'Usage', value: '**%help [command]**'},
-                            {name: '%help', value: 'Lists usable commands'},
-                            {name: '%help <command>', value: 'Displays help for a certain command'}
+                        authorname: 'Bot Help',
+                        title: 'Command: %help',
+                        thumbnail: helpIcon,
+                        fields: [
+                            {
+                                name: 'Usage',
+                                value: '**%help [command]**'
+                            },
+                            {
+                                name: '%help',
+                                value: 'Lists usable commands'
+                            },
+                            {
+                                name: '%help <command>',
+                                value:
+                                            'Displays help for a certain command'
+                            }
                         ],
-                        'color': 0x7ae576
+                        color: 0x7ae576
                     }
                 });
 
                 break;
-            case 'banmyself':
-            case '%banmyself':
-                bot.sendMessage({
-                    to: channelID,
-                    message: null,
-                    embed: {
-                        'authorname': 'Bot Help',
-                        'title': 'Command: %banmyself',
-                        'thumbnail': helpIcon,
-                        'fields': [
-                            {name: 'Usage', value: '**%banmyself**'},
-                            {name: '%banmyself', value: 'Bans the user.'},
-                        ],
-                        'color': 0x7ae576
-                    }
-                });
-                break;
+
             case 'hello':
             case '%hello':
                 bot.sendMessage({
                     to: channelID,
                     message: null,
                     embed: {
-                        'authorname': 'Bot Help',
-                        'title': 'Command: %hello',
-                        'thumbnail': helpIcon,
-                        'fields': [
-                            {name: 'Usage', value: '**%hello**'},
-                            {name: '%hello', value: 'Say hello to the bot!'}
+                        authorname: 'Bot Help',
+                        title: 'Command: %hello',
+                        thumbnail: helpIcon,
+                        fields: [
+                            { name: 'Usage', value: '**%hello**' },
+                            {
+                                name: '%hello',
+                                value: 'Say hello to the bot!'
+                            }
                         ],
-                        'color': 0x7ae576
+                        color: 0x7ae576
                     }
                 });
 
                 break;
+
             case 'randmon':
             case '%randmon':
                 bot.sendMessage({
-                to: channelID,
-                message: null,
-                embed: {
-                    'authorname': 'Bot Help',
-                    'title': 'Command: %randmon',
-                    'thumbnail': helpIcon,
-                    'fields': [
-                        {name: 'Usage', value: '**%randmon**'},
-                        {name: '%randmon', value: 'Generate a random Pokemon (no formes)'}
-                    ],
-                    'color': 0x7ae576
-                }
-            });
+                    to: channelID,
+                    message: null,
+                    embed: {
+                        authorname: 'Bot Help',
+                        title: 'Command: %randmon',
+                        thumbnail: helpIcon,
+                        fields: [
+                            { name: 'Usage', value: '**%randmon**' },
+                            {
+                                name: '%randmon',
+                                value:
+                                            'Generate a random Pokemon (no formes)'
+                            }
+                        ],
+                        color: 0x7ae576
+                    }
+                });
 
-            break;
+                break;
 
             default:
                 bot.sendMessage({
                     to: channelID,
                     message: null,
                     embed: {
-                        'authorname': 'Bot Help',
-                        'title': 'Command List',
-                        'thumbnail': helpIcon,
-                        'description': 'Bot: `%help`\nMisc: `%hello`',
-                        'footer': {'text': 'use %help <command> for command-specific help'},
-                        'color': 0x7ae576
+                        authorname: 'Bot Help',
+                        title: 'Command List',
+                        thumbnail: helpIcon,
+                        description: 'Bot: `%help`\nPokemon: `%randmon`\nMisc: `%hello`',
+                        footer: {
+                            text:
+                                        'use %help <command> for command-specific help'
+                        },
+                        color: 0x7ae576
                     }
                 });
             }
