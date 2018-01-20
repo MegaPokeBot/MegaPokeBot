e0ca9825f8fa9c585233a009bef03fa775eb0ba2
Matt <mmszek3000@gmail.com>
2018-01-20 09:28:01 -0500

    Me stupids; it's pokedex.js (not json)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.io');
 const logger = require('winston');
 const auth = require('./auth.json');
 const { listOfMons } = require('./listofmons.json');
-const dex = require('./dex-data/data/pokedex.json').BattlePokedex;
+const dex = require('./dex-data/data/pokedex.js').BattlePokedex;
 
 var helpIcon = {
     url: 'https://housing.umn.edu/sites/housing.umn.edu/files/help.png',
