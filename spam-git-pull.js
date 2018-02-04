5ff4f086ae5551a131952b6dfddd3d7d616c8dfa
Matt <mmszek3000@gmail.com>
2018-02-04 07:48:12 -0500

    update recursivley [server]

--- spam-git-pull.js
+++ spam-git-pull.js
@@ -1,10 +1,13 @@
 const { exec } = require('child_process');
 function gitPull() {
-    exec('git pull && git submodule update', (err, stdout, stderr) => {
-        if (err) {
-            // Quit if a current pull is already active
-            return;
+    exec(
+        'git pull && git submodule update --recursive',
+        (err, stdout, stderr) => {
+            if (err) {
+                // Quit if a current pull is already active
+                return;
+            }
         }
-    });
+    );
 }
 setInterval(gitPull, 10000);
